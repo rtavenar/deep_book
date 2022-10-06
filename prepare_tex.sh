f1ab30331e9d8fb8d2c97046c0df8b7ecfbf7708
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2022-10-06 16:05:56 +0200

    Bugfix

--- prepare_tex.sh
+++ prepare_tex.sh
@@ -3,6 +3,7 @@ for fname in content/*.md;
 do
     sed "s/%config InlineBackend.figure_format = 'svg'//g" ${fname} | sed "s/tex_specific_width:/width:/" | sed "s/is_html_output = True/is_html_output = False/" > ${fname/content/content_tex};
 done
+cp content/*.py content_tex/
 
 cp -R content/data content_tex/
 cp content/references.bib content_tex/
